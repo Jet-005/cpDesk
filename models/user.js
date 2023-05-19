e502d4b5b689135433df0454623b2f1c3ef8d873
Gao Zhu <gaozhu@g2l-service>
2023-05-19 18:14:44 +0800

    update

--- models/user.js
+++ models/user.js
@@ -13,6 +13,6 @@ module.exports = {
     updateTime: Date, //更新时间
     sessionKey: String, // session_key,用来检查登录是否失效
     connectId: String, // 关联的用户id,初始为""
-    role: Number, // 0 - 用户 1 - 商家
+    role: Number, // 0 - 用户 1 - 商家 2 - 未配对
   },
 }
