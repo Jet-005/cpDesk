6d82da59f6779f1800b24845b1e87883647a45bd
gaozhu@g2l-service <gaozhu@g2l-service.com>
2023-07-14 09:59:16 +0800

    update

--- controllers/order.js
+++ controllers/order.js
@@ -2,8 +2,34 @@
 const orderServices = require("../services").order;
 // const { checkUser } = require("../middlewares/middlewares")
 module.exports = {
-  "get,order/list/:page": {
-    name: "category list",
+  "get,order/mana/list/:page": {
+    name: "order list",
+    method: "get",
+    fun: [
+      // checkUser,
+      async (ctx, next) => {
+        try {
+          const { user } = ctx.state;
+          if (!user) return next();
+          const { page } = ctx.params;
+          const res = await orderServices.findAll(page, 5, user._id);
+          const total = await orderServices.count(user._id);
+          ctx.result = {
+            success: true,
+            msg: "ok",
+            code: 0,
+            data: res,
+            count: total,
+          };
+          return next();
+        } catch (error) {
+          console.error(error);
+        }
+      },
+    ],
+  },
+  "get,order/user/list/:page": {
+    name: "order list",
     method: "get",
     fun: [
       // checkUser,
--- models/orderDetail.js
+++ models/orderDetail.js
@@ -2,11 +2,10 @@
 module.exports = {
   name: 'order',
   schema: {
-    customerId: String, // 下单用户id
-    merchantID: String, // 关联的商家id
+    orderId: String, // 订单id
+    quantity: Number, // 商品的数量
     orderDate: String, // 订单日期
     totalAmount: Number, // 订单总金额
-    productID: Array, // 商品id
     orderStatus: String, // 订单状态 todo 需考虑流转状态的设计
     paymentStatus: String, // 支付状态 todo 需考虑流转状态的设计
     createTime: Date, // 创建时间
